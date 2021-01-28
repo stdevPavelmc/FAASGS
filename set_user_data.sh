a4ce41b84ec2643a03fcf5afadab923aad598afb
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2021-01-27 23:44:06 -0500

    Fix a path in the user confg script

--- set_user_data.sh
+++ set_user_data.sh
@@ -4,6 +4,7 @@
 . /etc/sat_data/user.conf
 
 # substitute LOC, LOC_NAME, LOC_COUNTRY in the index
-sed -i s/"%LOC%"/${LOC}/ /etc/var/www/html/index.php
-sed -i s/"%LOC_NAME%"/${LOC_NAME}/ /etc/var/www/html/index.php
-sed -i s/"%LOC_COUNTRY%"/${LOC_COUNTRY}/ /etc/var/www/html/index.php
+INDEX="/var/www/html/index.php"
+sed -i s/"%LOC%"/${LOC}/ ${INDEX}
+sed -i s/"%LOC_NAME%"/${LOC_NAME}/ ${INDEX}
+sed -i s/"%LOC_COUNTRY%"/${LOC_COUNTRY}/ ${INDEX}
